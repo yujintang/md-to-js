17409d55edf059a35813ca4605011ade89a97905
余金堂 <shanquan54@163.com>
2016-08-12 16:35:13 +0800

    add

---
+++ README.md
@@ -0,0 +1,25 @@
+# 使用markdown 生成js代码
+
+## 用法:
+
+下载:
+````
+npm install md-to-js --save
+````
+
+## api:
+
+### md生成数组对象
+````
+mj.transMd(url);
+````
+
+### 数组生成js文件
+````
+mj.writeJs(url, arr);
+````
+
+###
+````
+mj.all(mdfile, jsfile);
+````
---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+var file = require('./lib/file');
+var fs = require('fs');
+var promise = require('bluebird');
+var path = require('path');
+
+
+module.exports = {
+    transMd: file.transMd,
+    writeJs: file.writeJs,
+
+    all: function (mdfile, jsfile) {
+
+        let mdStat = fs.lstatSync(mdfile);
+        let jsStat = fs.lstatSync(jsfile);
+
+        if(!mdStat.isDirectory() || !jsStat.isDirectory()) {
+            console.log('参数必须为文件夹');
+            process.exit(1);
+        }
+
+        let redDir = fs.readdirSync(mdfile);
+        let arr = [];
+        for (let i of redDir) {
+            let url = path.join(mdfile, i);
+            arr.push(file.transMd(url));
+        }
+
+        return promise.all(arr)
+            .then(function (result) {
+                for(let i of result) {
+                    let fileName = (/(?:[\w\W]*\/([^/.]*).[\w\W]+)/.exec(i[1]))[1];
+                    let url = jsfile + fileName + '.js';
+                    file.writeJs(url, i[0]);
+                }
+            })
+    }
+};
