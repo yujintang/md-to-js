25e43ac03f8feff072a3dee4c14f5eff69f71496
余金堂 <shanquan54@163.com>
2016-08-17 13:16:47 +0800

    ad

---
+++ bin/md2js.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+"use strict";
+var index = require('../index');
+index.all(process.argv[2], process.argv[3]);
--- index.js
+++ index.js
@@ -11,33 +11,33 @@ module.exports = {
 
     all: function (mdfile, jsfile) {
 
-        if(!jsfile) {
-            jsfile = './js_file/';
+        if (!jsfile) {
+            jsfile = path.resolve(process.cwd(), './js_file/');
             if (!fs.existsSync(jsfile)) {
                 fs.mkdirSync(jsfile);
             }
         }
 
         let arr = [];
-        
-        
-           if(/[\w\W]+\.md$/.test(mdfile)) {
+
+
+        if (/[\w\W]+\.md$/.test(mdfile)) {
             arr.push(file.transMd(mdfile));
-            } else {
-               let redDir = fs.readdirSync(mdfile);
-               for (let i of redDir) {
-                   if (/[\w\W]+\.md/.test(i)) {
-                       let url = path.join(mdfile, i);
-                       arr.push(file.transMd(url));
-                   }
-               }
-           }
-        
+        } else {
+            let redDir = fs.readdirSync(mdfile);
+            for (let i of redDir) {
+                if (/[\w\W]+\.md/.test(i)) {
+                    let url = path.join(mdfile, i);
+                    arr.push(file.transMd(url));
+                }
+            }
+        }
+
         return promise.all(arr)
             .then(function (result) {
-                for(let i of result) {
+                for (let i of result) {
                     let fileName = path.basename(i[1], '.md');
-                    let url = jsfile + fileName + '.js';
+                    let url = path.join(jsfile, fileName + '.js');
                     file.writeJs(url, i[0]);
                 }
             })
