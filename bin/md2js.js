0d59151a4556cf62cbb8fa90451971d86728e3f1
余金堂 <shanquan54@163.com>
2016-10-27 11:42:01 +0800

    1.0.11

--- bin/md2js.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-"use strict";
-var index = require('../index');
-index.all(process.argv[2], process.argv[3]);
--- index.js
+++ index.js
@@ -5,17 +5,35 @@ var promise = require('bluebird');
 var path = require('path');
 
 module.exports = {
-    transMd: file.transMd,
-    writeJs: file.writeJs,
     createMd: file.createMd,
 
-    all: function (mdfile, jsfile) {
+    all: function (mdfile, js_file) {
+        
+        if (!js_file) {
+            let output = path.resolve(process.cwd(), './output/');
+            let mock_file = path.resolve(process.cwd(), './output/js_file/mock');
+            js_file = path.resolve(process.cwd(), './output/js_file');
 
-        if (!jsfile) {
-            jsfile = path.resolve(process.cwd(), './js_file/');
-            if (!fs.existsSync(jsfile)) {
-                fs.mkdirSync(jsfile);
+            if (!fs.existsSync(output)) {
+                fs.mkdirSync(output);
             }
+
+            if (!fs.existsSync(js_file)) {
+                fs.mkdirSync(js_file);
+            }
+
+            if (!fs.existsSync(mock_file)) {
+                fs.mkdirSync(mock_file);
+            }
+        }
+
+        if(!fs.existsSync(js_file)) {
+            console.log('js_file路径不存在');
+            process.exit(1);
+        }
+        if(!fs.existsSync(mdfile)) {
+            console.log('mdfile路径不存在');
+            process.exit(1);
         }
 
         let arr = [];
@@ -32,12 +50,12 @@ module.exports = {
                 }
             }
         }
-
+        
         return promise.all(arr)
             .then(function (result) {
                 for (let i of result) {
                     let fileName = path.basename(i[1], '.md');
-                    let url = path.join(jsfile, fileName + '.js');
+                    let url = path.join(js_file, fileName + '.js');
                     file.writeJs(url, i[0]);
                 }
             })
